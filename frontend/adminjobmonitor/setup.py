5f0b4f102bef8b09e9db301ed50b410d8d44e4a7
Thomas Röhl <thomas.roehl@fau.de>
2017-07-18 14:15:07 +0200

    Deletions and rename of adminjobmonitor

--- frontend/adminjobmonitor/setup.py
+++ frontend/adminjobmonitor/setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup
 from glob import glob
 
-setup(name='AdminJobMonitor',
+setup(name='adminjobmonitor',
       version='0.1',
       description='Listen to signal packets of the LMS and create the admin view',
       author='Thomas Roehl',
       author_email='Thomas.Roehl@fau.de',
-      packages=["AdminJobMonitor"],
+      packages=["adminjobmonitor"],
       data_files=[("etc" , ["AdminJobMonitor.conf"])],
       entry_points={
         'console_scripts': [
-            'adminJobMonitor=AdminJobMonitor.AdminJobMonitor:main'
+            'adminJobMonitor=adminjobmonitor.adminjobmonitor:main'
             ]}
      )
